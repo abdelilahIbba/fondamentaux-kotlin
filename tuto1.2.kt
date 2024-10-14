154f49136af3e75f5166b668ed5897936e31561f
abdelilahIbba <2001081800535@ofppt-edu.ma>
2024-10-14 13:11:45 +0100

    Utiliser la possibilité de valeur nulle en Kotlin

--- tuto1.2.kt
+++ tuto1.2.kt
@@ -10,10 +10,28 @@
 //    println(number)
 //}
 
-fun main() {
-    var favoriteActorr: String = "Sandra Oh"
-    println(favoriteActorr.length)
+//fun main() {
+//    var favoriteActor: String = "Sandra Oh"
+//
+////    println(favoriteActor.length)
+////    println(favoriteActor?.length)  // fach kayt7dd type d var mymknch ikon null
+////    println(favoriteActor!!.length) // kanzidoha f7alt kna mt2kdin var machi null
+//}
 
+//fun main() {
+//    var favoriteActor: String? = null
+//
+//    if (favoriteActor != null) {
+//        println("The number of characters in your favorite actor's name is ${favoriteActor.length}.")
+//    } else {
+//        println("You didn't input a name.")
+//    }
+//}
+
+fun main() { // ida kan (favoriteActor = null) kay rj3 0 blast error
     var favoriteActor: String? = "Sandra Oh"
-    println(favoriteActor?.length)
+    val lengthOfName = favoriteActor?.length ?: 0  // elvis operator
+
+    println("The number of characters in your favorite actor's name is $lengthOfName.")
 }
+
